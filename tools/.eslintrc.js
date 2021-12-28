71726d0881669fc1cd0b1df80aa1e0c23cc72276
Phillip Rearick <psrearick@gmail.com>
2021-12-28 17:30:42 -0500

    install eslint

---
+++ tools/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    env: {
+        // this section will be used to determine which APIs are available to us
+        // (i.e are we running in a browser environment or a node.js env)
+        node: true,
+        browser: true,
+    },
+    globals: {
+        window: true,
+        axios: true,
+        route: true,
+        _: "readonly",
+    },
+    parserOptions: {
+        parser: "babel-eslint",
+        // specifying a module sourcetype prevent eslint from marking import statements as errors
+        sourceType: "module",
+    },
+    plugins: ["lodash"],
+    extends: [
+        // use the recommended rule set for both plain javascript and vue
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended",
+        "plugin:prettier/recommended",
+        // "plugin:lodash/recommended",
+    ],
+    ignorePatterns: ["ziggy.js"],
+    rules: {
+        // we should always disable console logs and debugging in production
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "vue/no-unused-components": "off",
+        "vue/no-v-html": "off",
+    },
+};
