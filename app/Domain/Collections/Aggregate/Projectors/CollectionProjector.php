e498126f9b5de2b948a4ccf5e3f3e54c4c5dcb32
Phillip Rearick <psrearick@gmail.com>
2022-01-01 09:11:42 -0500

    Add collection card summary projector

--- app/Domain/Collections/Aggregate/Projectors/CollectionProjector.php
+++ app/Domain/Collections/Aggregate/Projectors/CollectionProjector.php
@@ -8,6 +8,7 @@ use App\Domain\Collections\Aggregate\Events\CollectionDeleted;
 use App\Domain\Collections\Aggregate\Events\CollectionMoved;
 use App\Domain\Collections\Aggregate\Events\CollectionUpdated;
 use App\Domain\Collections\Models\Collection;
+use App\Domain\Collections\Models\CollectionCardSummary;
 use Carbon\Carbon;
 use Spatie\EventSourcing\EventHandlers\Projectors\Projector;
 
@@ -16,15 +17,8 @@ class CollectionProjector extends Projector
     public function onCollectionCardUpdated(CollectionCardUpdated $event) : void
     {
         $attributes = $event->collectionCardAttributes;
-        Collection::uuid($attributes['uuid'])->cards()
-            ->attach($attributes['updated']['id'], [
-                'collection_uuid'  => $attributes['uuid'],
-                'card_uuid'        => $attributes['updated']['uuid'],
-                'price_when_added' => $attributes['updated']['acquired_price'],
-                'quantity'         => $attributes['quantity_diff'],
-                'finish'           => $attributes['updated']['finish'],
-                'date_added'       => Carbon::now(),
-            ]);
+        $this->updateCollectionCard($attributes);
+        $this->updateCollectionCardSummary($attributes);
     }
 
     public function onCollectionCreated(CollectionCreated $event) : void
@@ -48,5 +42,50 @@ class CollectionProjector extends Projector
     {
         $attributes = $event->collectionAttributes;
         Collection::uuid($attributes['uuid'])->update($attributes);
+    }
+
+    private function updateCollectionCard(array $attributes) : void
+    {
+        Collection::uuid($attributes['uuid'])->cards()
+            ->attach($attributes['updated']['id'], [
+                'collection_uuid'  => $attributes['uuid'],
+                'card_uuid'        => $attributes['updated']['uuid'],
+                'price_when_added' => $attributes['updated']['acquired_price'],
+                'quantity'         => $attributes['quantity_diff'],
+                'finish'           => $attributes['updated']['finish'],
+                'date_added'       => Carbon::now(),
+            ]);
+    }
+
+    private function updateCollectionCardSummary(array $attributes) : void
+    {
+        $cardUuid   = $attributes['updated']['uuid'];
+        $finish     = $attributes['updated']['finish'];
+        $change     = $attributes['quantity_diff'];
+        $price      = $attributes['updated']['acquired_price'];
+
+        $existingCard = CollectionCardSummary::where('collection_uuid', '=', $attributes['uuid'])
+            ->where('card_uuid', '=', $cardUuid)
+            ->where('finish', '=', $finish)
+            ->first();
+        
+        if (!$existingCard) {
+            CollectionCardSummary::create([
+                'collection_uuid'       => $attributes['uuid'],
+                'card_uuid'             => $cardUuid,
+                'price_when_added'      => $price,
+                'price_when_updated'    => $price,
+                'quantity'              => $change,
+                'finish'                => $finish,
+                'date_added'            => Carbon::now(),
+            ]);
+
+            return;
+        }
+
+        $existingCard->update([
+            'price_when_updated'    => $price,
+            'quantity'              => $existingCard->quantity + $change,
+        ]);
     }
 }
---
+++ app/Domain/Collections/Models/CollectionCardSummary.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Domain\Collections\Models;
+
+use App\Domain\Base\Model;
+
+class CollectionCardSummary extends Model
+{
+    protected $guarded = [];
+}
