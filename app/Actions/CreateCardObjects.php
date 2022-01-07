f7243d03f121ba30e663fbb1a6e0d120833e0f56
Phillip Rearick <psrearick@gmail.com>
2022-01-07 10:38:22 -0500

    move card object create to job

--- app/Actions/CreateCardObjects.php
+++ app/Actions/CreateCardObjects.php
@@ -2,19 +2,12 @@
 
 namespace App\Actions;
 
-use App\Domain\Cards\Models\Card;
-use App\Jobs\CreateCardSearchDataObjects;
+use App\Jobs\ChunkCardObjectCreate;
 
 class CreateCardObjects
 {
     public function __invoke()
     {
-        Card::with(['frameEffects', 'set', 'finishes', 'prices'])->chunk(30,
-            function ($cards) {
-                $cards->each(function ($card) {
-                    CreateCardSearchDataObjects::dispatch($card);
-                });
-            }
-        );
+        ChunkCardObjectCreate::dispatch();
     }
 }
---
+++ app/Jobs/ChunkCardObjectCreate.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Domain\Cards\Models\Card;
+use App\Jobs\CreateCardSearchDataObjects;
+
+class ChunkCardObjectCreate implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Card::with(['frameEffects', 'set', 'finishes', 'prices'])->chunk(30,
+            function ($cards) {
+                $cards->each(function ($card) {
+                    CreateCardSearchDataObjects::dispatch($card);
+                });
+            }
+        );
+    }
+}
